40808dfd738307558bc8fcbfef46cb976cb83e88
Harivijay Chormage <hbchormage97@gmail.com>
2024-12-11 10:13:02 +0530

    Add files via upload
    please, find the attachment for your reference.

---
+++ Streams.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Streams {
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<>(20);
+        name.add("Ahmedabad");
+        name.add("Ahilyanagar");
+        name.add("Akola");
+        name.add("Amravati");
+        name.add("Beed");
+        name.add("Latur");
+        name.add("Jalna");
+        name.add("Anand");
+        name.add("Thane");
+        name.add("Pune");
+        name.add("Mumbai");
+        name.add("Jalgaon");
+        name.add("Dharashiv");
+        name.add("Saswad");
+        name.add("Purandar");
+        name.add("Baramati");
+        name.add("Sangamner");
+        name.add("Agra");
+        name.add("Amritsar");
+        name.add("Ajmer");
+
+        Collections.sort(name);
+        for(int i=0; i<name.size(); i++) {
+            if(name.get(i).contains("A")) {
+                System.out.println(name.get(i) + "->" + name.get(i).length());
+            }
+        }
+    }
+}
